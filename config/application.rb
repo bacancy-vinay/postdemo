19808a9f5907789e704ec81146bece1cd9e7cbb3
bacancy-vinay <vinay.sharma@bacancy.com>
2022-07-08 12:33:35 +0530

    fix issue

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
+    config.secret_key_base = 'blipblapblup'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
